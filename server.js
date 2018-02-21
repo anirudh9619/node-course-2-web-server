b0d93b91505966e2dd504b76ce223eda721ae47b
Anirudh gangwar <anirudhgangwar444@gmail.com>
2018-02-22 00:00:32 +0530

    Initial comment

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const hbs= require('hbs');
+const fs=require('fs');
+var app=express();
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname +'/views/partials');
+app.set('view engine', 'hbs');
+hbs.registerHelper('getCurrentYear', ()=> {
+  return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+// app.use((req,res,next)=>{
+//   res.render('maintenance.hbs');
+// });
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log=`${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log + '\n');
+next();
+});
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+    pageTitle: `Home`,
+    welcome: 'Hii this your page',
+    CurrentYear:new Date().getFullYear()
+  });
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle: `About Page`
+  });
+
+});
+app.get('/Bad',(req,res)=>{
+  res.send({
+    errorMessage:'Unable to fullfill your request'
+  });
+});
+app.listen(3000);
